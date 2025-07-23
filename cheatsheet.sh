501b3539995c6c07cd5ce9321f67f3cafa1bcfce
Siwat Chad Ruangroengkulrith <siwat.rua@gmail.com>
2025-07-23 17:53:26 +0200

    Cheatsheet: reset

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,10 +1,23 @@
-# Rebase
+#region MARK: Hard Reset
+# Hard reset HEAD to branch
+git reset --hard origin/main
+
+# Hard reset to specific commit
+git reset --hard <commit-hash>
+
+# NOTE: Normally git reset will simply unstage the file if the file name is given
+# To unstage all file in HEAD:
+git reset HEAD --
+
+#region MARK: Rebase
 # Assume we have three branches already created: main, b1, b2
 git rebase --onto main b1
 git rebase --onto main b1 b2
 
 # To abort the rebase operation - i.e., in case of conflict
 git rebase --abort
+
+#region MARK: Branches
 
 # Remove local branch
 git branch new_branch # make new one
@@ -14,3 +27,4 @@ git branch -d new_branch
 git branch -a
 git remote update origin --prune
 git branch -a
+
