d5e62b3ac51dd30d89880b2bd15e0f63c602c9c7
Siwat Chad Ruangroengkulrith <siwat.rua@gmail.com>
2025-07-23 17:44:18 +0200

    cheatsheet: remove branch

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,3 +1,7 @@
+# Remove local branch
+git branch new_branch # make new one
+git branch -d new_branch
+
 # Update list of tracking remote branch on local machine
 git branch -a
 git remote update origin --prune
