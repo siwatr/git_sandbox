817f6e80007d64764107504b5def30651923357a
Siwat Chad Ruangroengkulrith <siwat.rua@gmail.com>
2025-07-23 17:47:07 +0200

    Cheatsheet: Rebase note

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,3 +1,11 @@
+# Rebase
+# Assume we have three branches already created: main, b1, b2
+git rebase --onto main b1
+git rebase --onto main b1 b2
+
+# To abort the rebase operation - i.e., in case of conflict
+git rebase --abort
+
 # Remove local branch
 git branch new_branch # make new one
 git branch -d new_branch
